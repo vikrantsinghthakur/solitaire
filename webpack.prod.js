254fa226fded27804ed807029e3fc3480dda0e8d
vikrant <thakur.vikrantsingh6000@gmail.com>
2018-02-22 22:13:31 +0530

    master: remove bundleAnalyzer from prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,12 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
    plugins: [
      new UglifyJSPlugin(),
-     new BundleAnalyzerPlugin(),
+//     new BundleAnalyzerPlugin(),
      new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
      }),
